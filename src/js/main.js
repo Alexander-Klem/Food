2734c4a296a015f6619eb6acde7720c74a9d3bc3
Александр Клемято <a.klemyato@rsm.by>
2024-10-10 15:40:25 +0300

    Лекция - 83

--- src/js/main.js
+++ src/js/main.js
@@ -178,14 +178,14 @@ window.addEventListener(`DOMContentLoaded`, () => {
             hours = 0;
             minutes = 0;
             seconds = 0;
-        } else { 
+        } else {
 
             days = Math.floor(t / (1000 * 60 * 60 * 24)),
  
-            hours = Math.floor((t / (1000 * 60 * 60) % 24)),
+                hours = Math.floor((t / (1000 * 60 * 60) % 24)),
 
-            minutes = Math.floor((t / 1000 / 60) % 60),
-            seconds = Math.floor((t / 1000) % 60);
+                minutes = Math.floor((t / 1000 / 60) % 60),
+                seconds = Math.floor((t / 1000) % 60);
         }
            
         
@@ -195,13 +195,13 @@ window.addEventListener(`DOMContentLoaded`, () => {
             'hours': hours,
             'minutes': minutes,
             'seconds': seconds
-    };
+        };
     }
     
-    function getZero(num) { 
+    function getZero(num) {
         if (num >= 0 && num < 10) {
             return `0${num}`;
-        } else { 
+        } else {
             return num;
         }
     }
@@ -238,15 +238,15 @@ window.addEventListener(`DOMContentLoaded`, () => {
     //Tabs
     
     const modalOpenBtn = document.querySelectorAll(`[data-modal]`),
-          modal = document.querySelector(`.modal`),
-          modalCloseBtn = document.querySelector(`[data-close]`);
+        modal = document.querySelector(`.modal`),
+        modalCloseBtn = document.querySelector(`[data-close]`);
     
     // Вариант без использования toggle
     modalOpenBtn.forEach(btn => {
         btn.addEventListener(`click`, openModal);
     });
 
-    function openModal() { 
+    function openModal() {
         modal.classList.add(`show`, `fade`);
         modal.classList.remove(`hide`);
         document.body.style.overflow = `hidden`;
@@ -255,21 +255,21 @@ window.addEventListener(`DOMContentLoaded`, () => {
 
     modalCloseBtn.addEventListener(`click`, closeModal);
 
-    function closeModal() { 
+    function closeModal() {
         modal.classList.add(`hide`);
         modal.classList.remove(`show`);
         document.body.style.overflow = `visible`;
     }
 
-    modal.addEventListener(`click`, (event) => { 
+    modal.addEventListener(`click`, (event) => {
         const target = event.target;
         if (target && target === modal) {
             closeModal();
-        } 
+        }
     });
 
     document.addEventListener(`keydown`, (event) => {
-        if (event.code === `Escape` && modal.classList.contains(`show`)) { 
+        if (event.code === `Escape` && modal.classList.contains(`show`)) {
             closeModal();
         }
     });
@@ -279,8 +279,8 @@ window.addEventListener(`DOMContentLoaded`, () => {
     // pageYOffset - это свойство отслеживает сколько px сверху у нас отлистал пользователь по оси Y (window.pageYOffset – то же самое, что и window.scrollY.)
     // Чтобы удалить какой-то обработчик события, мы должны делать ссылку именно функцию, которая исполнялась как этот обработчик - removeEventListener();
     
-    function showModalByScroll() { 
-        if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight -1) { 
+    function showModalByScroll() {
+        if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight - 1) {
             openModal();
             window.removeEventListener(`scroll`, showModalByScroll);
         }
@@ -302,7 +302,7 @@ window.addEventListener(`DOMContentLoaded`, () => {
 
     //Используем класс для карточек (Лекция - 78)
 
-    class MenuCard { 
+    class MenuCard {
         constructor(src, alt, title, descr, price, parentSelector, ...clasess) {
             this.src = src;
             this.alt = alt;
@@ -312,11 +312,11 @@ window.addEventListener(`DOMContentLoaded`, () => {
             this.clasess = clasess;
             this.parent = document.querySelector(parentSelector);
             this.transfer = 27;
-            this.changeToUAH(); 
+            this.changeToUAH();
         }
 
         changeToUAH() {
-            this.price = this.price * this.transfer; 
+            this.price = this.price * this.transfer;
         }
 
         render() {
@@ -327,8 +327,8 @@ window.addEventListener(`DOMContentLoaded`, () => {
                 // element.classList.add(this.neweElement);
                 this.element = `menu__item`;
                 element.classList.add(this.element);
-            } else { 
-                this.clasess.forEach(className => element.classList.add(className)); 
+            } else {
+                this.clasess.forEach(className => element.classList.add(className));
             }
             
             
@@ -389,7 +389,7 @@ window.addEventListener(`DOMContentLoaded`, () => {
     
     // Объект может существовать без переменной - делается это тогда, когда этот объект используется только на месте т.е если мы его сейчас в переменную никакую не положим, то потом он просто потеряется т.е создаться и удалиться т.к на него не будет потом просто никаких ссылок.
 
-    //Forms 
+    /*Forms (Недоделанная попытка)
 
     const forms = document.querySelectorAll(`form`); 
 
@@ -437,9 +437,178 @@ window.addEventListener(`DOMContentLoaded`, () => {
                 // Когда мы работаем с локальным сервером, то мы каждый раз после каких-то изменений должны сбрасывать кэш. Делается для того, чтобы новые изменения применились. (Shift + F5)
             })
         })
-    }
+    }*/
 
-}); 
+    
+    //Forms 
+
+    /*Первый пример без использования JSON
+     const forms = document.querySelectorAll(`form`);
+ 
+     // По сути это небольшое хранилище сообщений, которых мы хотим показать пользователю
+     const message = {
+         loading: `Загрузка`,
+         success: `Спасибо! Скоро мы с вами свяжемся`,
+         failure: `Что-то пошло не так...`
+     };
+ 
+     forms.forEach(item => { 
+         postData(item);
+     })
+ 
+     function postData(form) { 
+         form.addEventListener(`submit`, (event) => {    
+             event.preventDefault();
+ 
+             //(message) Чтобы поместить message, мы динимачески создадим новый блок на странице и выводим сообщение(картинку, текс и т.д), чаще всего он добавляется к форме.
+ 
+             const statusMesage = document.createElement(`div`);
+             statusMesage.classList.add(`status`);
+             statusMesage.textContent = message.loading;
+             form.append(statusMesage);
+ 
+ 
+             const request = new XMLHttpRequest();
+             request.open(`POST`, `server.php`);
+             
+             // Как сделать так, чтобы все данные которые заполнил пользователь в форме, мы получили в JS и смогли отправить на сервер. Конечно мы в ручную могли бы взять эту форму, взять все input и их value, перебрать, сформировать объект. Но это запарное занятие и у нас есть готовый механизм для этого всего. Самый простой способ - это использовать объект FormData (нам не всегда нужно передавать в формате JSON. Завивисит от того с какой технологией с который мы работаем на Backend).;
+ 
+             // FormData(откуда берем данные) - это специальный объект, который позволяет с определенной формы быстро сформировать данные которые заполнил пользователь(ключ, значение). 
+             
+             // !Важный момент, на котором можно запнуться - это зависит от добросовестности верстальщика и касается того, как сверстаны формы и как прописаны input. Самое главное, что когда мы подрузумеваем, что эти данные будут идти на сервер, мы должны АБСОЛЮТНО ВСЕГДА! указывать атрибут name (name = `неважно какой текст(name)`). Иначе FormData не сможет найти этот input и не сможет найти ему нужные данные.
+             
+             // request.setRequestHeader(`Content-type`, `multipart/form-data`);
+              // !Когда мы используем связку XMLHttpRequest + FormData, то нам заголовок устанавливать не нужно т . к он устанавливается автоматически(именно из-за этой проблемы, мы не получили данные на сервере). Значит request.setRequestHeader устанавливать не нужно!!
+ 
+             
+             // `multipart/form-data` - чтобы работать с formData, нам нужно установить такой заголовок.
+ 
+             const formData = new FormData(form);
+ 
+             request.send(formData);
+ 
+             request.addEventListener('load', () => {
+                 if (request.status === 200) {
+                     console.log(request.response);
+                     statusMesage.textContent = message.success;
+                     form.reset();
+                     setTimeout(() => {
+                         statusMesage.remove();
+                     }, 2000);
+                 } else {
+                     statusMesage.textContent = message.failure;
+                 }
+             });
+         })
+     }
+ 
+     // !Важный момент. Когда мы работаем на локальном сервере, то придется каждый раз после каких-то изменений сбрасывать кэш. Делается для того, чтобы все изменения применились на странице, потому что сейчас сервер запонимает старые изменения, чтобы их каждый раз не подгружать и это называется - кэш. (Комбинация для очистки: Shift + F5)*/
+
+    // 2) Но если мы хотим, чтобы наш сервер принимал данные не в обычном формате, а в JSON.Значит нужны данные в формате JSON.
+    
+    // const forms = document.querySelectorAll('form');
+    // const message = {
+    //     loading: 'Загрузка...',
+    //     success: 'Спасибо! Скоро мы с вами свяжемся',
+    //     failure: 'Что-то пошло не так...'
+    // };
+
+    // forms.forEach(item => {
+    //     postData(item);
+    // });
+
+    // function postData(form) {
+    //     form.addEventListener('submit', (e) => {
+    //         e.preventDefault();
+
+    //         let statusMessage = document.createElement('div');
+    //         statusMessage.classList.add('status');
+    //         statusMessage.textContent = message.loading;
+    //         form.appendChild(statusMessage);
+        
+    //         const request = new XMLHttpRequest();
+    //         request.open('POST', 'server.php');
+    //         request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+
+    //         const formData = new FormData(form);
+
+    //         // У нас есть formData, который необходимо превратить в формат JSON(formData специфический объект и мы не можем просто так его прогнать в другой формат). Используем следующий прием
+
+    //         const object = {};
+    //         formData.forEach(function (value, key) {
+    //             object[key] = value;
+    //         });
+    //         const json = JSON.stringify(object);
+
+    //         request.send(json);
+
+    //         // Есть небольшой нюанс с Backend разработкой. PHP не умеет работать с форматами данных JSON, чаще всего будем отправлять данные на сервера с использованием nodeJS. Но все равно с таким типо данных можно поработать (смотри server.php)
+
+    //         request.addEventListener('load', () => {
+    //             if (request.status === 200) {
+    //                 console.log(request.response);
+    //                 statusMessage.textContent = message.success;
+    //                 form.reset();
+    //                 setTimeout(() => {
+    //                     statusMessage.remove();
+    //                 }, 2000);
+    //             } else {
+    //                 statusMessage.textContent = message.failure;
+    //             }
+    //         });
+    //     });
+    // }
+    
+    const forms = document.querySelectorAll('form');
+    const message = {
+        loading: 'Загрузка...',
+        success: 'Спасибо! Скоро мы с вами свяжемся',
+        failure: 'Что-то пошло не так...'
+    };
+
+    forms.forEach(item => {
+        postData(item);
+    });
+
+    function postData(form) {
+        form.addEventListener('submit', (e) => {
+            e.preventDefault();
+
+            let statusMessage = document.createElement('div');
+            statusMessage.classList.add('status');
+            statusMessage.textContent = message.loading;
+            form.appendChild(statusMessage);
+        
+            const request = new XMLHttpRequest();
+            request.open('POST', 'server.php');
+            request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+            const formData = new FormData(form);
+
+            const object = {};
+            formData.forEach(function(value, key){
+                object[key] = value;
+            });
+            const json = JSON.stringify(object);
+
+            request.send(json);
+
+            request.addEventListener('load', () => {
+                if (request.status === 200) {
+                    console.log(request.response);
+                    statusMessage.textContent = message.success;
+                    form.reset();
+                    setTimeout(() => {
+                        statusMessage.remove();
+                    }, 2000);
+                } else {
+                    statusMessage.textContent = message.failure;
+                }
+            });
+        });
+    }
+});
+    
+
 
 
 
