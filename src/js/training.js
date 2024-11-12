d88e304265c7443237c5168089f8d8aec9dffd67
Alexander <alexander_klem@mail.ru>
2024-11-12 22:21:41 +0300

    Лекция 93.1(training)

--- src/js/training.js
+++ src/js/training.js
@@ -441,12 +441,12 @@ window.addEventListener('DOMContentLoaded', function () {
     };
 
     /*1-й вариант создания карточек*/
-    //     getResourses(`http://localhost:3000/menu`)
-    //         .then(data => {
-    //             data.forEach(({img, altimg, title, descr, price}) => {
-    // new MenuCard(img, altimg, title, descr, price, `.menu .container`).render();
-    //             });
-    //         })
+        getResourses(`http://localhost:3000/menu`)
+            .then(data => {
+                data.forEach(({img, altimg, title, descr, price}) => {
+    new MenuCard(img, altimg, title, descr, price, `.menu .container`).render();
+                });
+            })
     
     /*2-й вариант создания карточек (если один раз будут использоваться)
         getResourses(`http://localhost:3000/menu`)
@@ -569,8 +569,6 @@ window.addEventListener('DOMContentLoaded', function () {
 
             const json = JSON.stringify(Object.fromEntries(formData.entries()));
 
-            
-           
             postData('http://localhost:3000/requests', json)
                 .then(data => {
                     console.log(data);
@@ -609,7 +607,7 @@ window.addEventListener('DOMContentLoaded', function () {
             closeModal();
         }, 4000)
     }
-
+    /*Слайдер (1-й вариант)
     const slides = document.querySelectorAll(`.offer__slide`),
         prev = document.querySelector(`.offer__slider-prev`),
         next = document.querySelector(`.offer__slider-next`),
@@ -655,6 +653,82 @@ window.addEventListener('DOMContentLoaded', function () {
 
     prev.addEventListener(`click`, () => { 
         plusSlides(-1)
+    })*/
+    
+    /*Слайдер (2-й вариант)*/
+
+    const slides = document.querySelectorAll(`.offer__slide`),
+          prev = document.querySelector(`.offer__slider-prev`),
+          next = document.querySelector(`.offer__slider-next`),
+          total = document.querySelector(`#total`),
+          current = document.querySelector(`#current`);
+          slidesWrapper = document.querySelector(`.offer__slider-wrapper`),
+          slidesField = document.querySelector(`.offer__slider-inner`),
+          width = window.getComputedStyle(slidesWrapper).width;
+    
+    let slideIndex = 1;
+    let offset = 0;
+
+    if (slides.length < 10) {
+        total.textContent = `0${slides.length}`;
+        current.textContent = `0${slideIndex}`;
+    } else { 
+        total.textContent = slides.length;
+        current.textContent = slideIndex;
+    }
+
+    slidesField.style.width = 100 * slides.length + '%';
+    slidesField.style.display = `flex`;
+    slidesField.style.transition = `0.5s all`;
+
+    slidesWrapper.style.overflow = `hidden`;
+
+    slides.forEach(item => { 
+        item.style.width = width;
     })
     
+    next.addEventListener(`click`, (event) => {
+        if (offset == +width.slice(0, width.length - 2) * (slides.length - 1)) {
+            offset = 0;
+        } else { 
+            offset += +width.slice(0, width.length - 2)
+        }
+        slidesField.style.transform = `translateX(-${offset}px)`;
+
+
+        if (slideIndex === slides.length) {
+            slideIndex = 1;
+        } else { 
+            slideIndex++;
+        }
+
+        if (slides.length < 10) {
+            current.textContent = `0${slideIndex}`;
+        } else { 
+            current.textContent = slideIndex;
+        }
+    });
+
+    prev.addEventListener(`click`, (event) => { 
+        if (offset == 0) {
+            offset = +width.slice(0, width.length - 2) * (slides.length - 1);
+        } else { 
+            offset -= +width.slice(0, width.length - 2);
+        }
+        slidesField.style.transform = `translateX(-${offset}px)`;
+
+        if (slideIndex == 1) {
+            slideIndex = slides.length;
+        } else {
+            slideIndex--;
+        }
+    
+        if (slides.length < 10) {
+            current.textContent = `0${slideIndex}`;
+        } else {
+            current.textContent = slideIndex;
+        }
+    })
+    
+
 });
